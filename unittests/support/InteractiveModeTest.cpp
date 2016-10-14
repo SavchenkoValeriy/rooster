51a2af6295babe5ffc4b6b223b7e1f9d7692ee1f
Valeriy Savchenko <sinmipt@gmail.com>
2016-10-15 01:30:24 +0300

    Add the first test using mocks.

--- unittests/support/InteractiveModeTest.cpp
+++ unittests/support/InteractiveModeTest.cpp
@@ -22,17 +22,17 @@ namespace {
   class InputReaderMock : public InputReader {
   public:
     static void init() { mock = std::make_unique<InputReaderMockObject>(); }
+    static void clear() { mock = nullptr;  }
     static CommandArgsContainer getArguments(const std::string &);
     static std::string getCommand(const std::string &);
-  private:
     static std::unique_ptr<InputReaderMockObject> mock;
   };
 
   class InputProviderMock : public InputProvider {
   public:
     static void init() { mock = std::make_unique<InputProviderMockObject>(); }
-    std::string read() { return mock->read(); }
-  private:
+    static void clear() { mock = nullptr;  }
+    static std::string read() { return mock->read(); }
     static std::unique_ptr<InputProviderMockObject> mock;
   };
 
@@ -45,4 +45,45 @@ namespace {
 
   std::unique_ptr<InputReaderMockObject> InputReaderMock::mock;
   std::unique_ptr<InputProviderMockObject> InputProviderMock::mock;
+
+  class CallbackProvider {
+  public:
+    MOCK_METHOD0(simple, void());
+  };
+}
+
+using ::testing::Return;
+using ::testing::_;
+
+using InteractiveModeUnderTest = InteractiveMode<InputReaderMock, InputProviderMock>;
+
+class InteractiveModeTest : public ::testing::Test {
+ protected:
+  virtual void SetUp() override {
+    InputReaderMock::init();
+    InputProviderMock::init();
+  }
+
+  virtual void TearDown() override {
+    InputReaderMock::clear();
+    InputProviderMock::clear();
+  }
+};
+
+TEST_F(InteractiveModeTest, SimpleCallbackTest) {
+  CallbackProvider mock;
+  InteractiveModeUnderTest tested;
+  CallbackTy<void> simpleCallback = [&mock]() { return mock.simple(); };
+  tested.registerCallback("simple", simpleCallback);
+
+  EXPECT_CALL(*InputProviderMock::mock, read())
+    .WillRepeatedly(Return(""));
+  EXPECT_CALL(*InputReaderMock::mock, getCommand(_))
+    .WillOnce(Return("simple"))
+    .WillOnce(Return("exit"));
+  EXPECT_CALL(*InputReaderMock::mock, getArguments(_));
+  EXPECT_CALL(mock, simple())
+    .Times(1);
+
+  tested.run();
 }
