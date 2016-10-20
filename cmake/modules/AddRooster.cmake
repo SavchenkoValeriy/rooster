e98d603b0c3ff6c0c414beacc5358950ad5428b9
Valeriy Savchenko <sinmipt@ispras.ru>
2016-10-20 23:19:09 +0300

    Get rid of '-fno-exceptions'.

--- cmake/modules/AddRooster.cmake
+++ cmake/modules/AddRooster.cmake
@@ -1,6 +1,6 @@
 # Generic support for adding a unittest.
 function(add_unittest test_suite test_name)
-  add_llvm_executable(${test_name} ${ARGN})
+  add_rooster_executable(${test_name} ${ARGN})
   set(outdir ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_CFG_INTDIR})
   set_output_directory(${test_name} BINARY_DIR ${outdir} LIBRARY_DIR ${outdir})
   target_link_libraries(${test_name} gtest_main)
@@ -14,4 +14,20 @@ function(add_unittest test_suite test_name)
   if (NOT ${test_suite_folder} STREQUAL "NOTFOUND")
     set_property(TARGET ${test_name} PROPERTY FOLDER "${test_suite_folder}")
   endif ()
+endfunction()
+
+function(rooster_update_compile_flags name)
+  set_property(TARGET ${name} APPEND_STRING PROPERTY
+    COMPILE_FLAGS "-fno-rtti")
+  set_property(TARGET ${name} PROPERTY CXX_STANDARD 14)
+endfunction()
+
+function(add_rooster_executable name)
+  add_executable(${name} ${ARGN})
+  rooster_update_compile_flags(${name})
+endfunction()
+
+function(add_rooster_library name)
+  add_library(${name} ${ARGN})
+  rooster_update_compile_flags(${name})
 endfunction()
